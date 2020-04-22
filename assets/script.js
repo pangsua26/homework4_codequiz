693860bb1f65a0db244a6659dee46198f1eb28fa
Pangsua Vang <pangsua26@yahoo.com>
2020-04-22 16:58:39 -0500

    added elements

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+var start = document.getElementById("start");
+var quiz = document.getElementById("quiz");
+var question = document.getElementById("question");
+var counter = document.getElementById("counter");
+var timer = document.getElementById("timer");
