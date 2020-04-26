a928483da2e3b91d6714f262dc626de4b443df7e
Pangsua Vang <pangsua26@yahoo.com>
2020-04-25 20:54:53 -0500

    added questions and answers

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
+// Select elements by ID
+
 var startEl = document.getElementById("start");
 var quizEl = document.getElementById("quiz");
 var timerEl = document.getElementById("timer");
@@ -7,9 +9,51 @@ var choice2 = document.getElementById("choice2");
 var choice3 = document.getElementById("choice3");
 var choice4 = document.getElementById("choice4");
 
+// Create questions and answers and correct answer
 let questions = [
     {
-        question : "Commonly used data types DO NOT include:",
-        choice1 : 
-    }
+        question: "Commonly used data types DO NOT include:",
+        choice1: "strings",
+        choice2: "booleans",
+        choice3: "alerts",
+        choice4: "numbers",
+        correct: "choice4"
+    },
+
+    {
+        question: "The condition in an if/else statement is enclosed within ______.",
+        choice1: "quotes",
+        choice2: "curly brackets",
+        choice3: "parenthesis",
+        choice4: "square brackets",
+        correct: "choice2"
+    },
+
+    {
+        question: "Arrays in JavaScript can be used to store ______.",
+        choice1: "numbers and strings",
+        choice2: "other arrays",
+        choice3: "booleans",
+        choice4: "all the above",
+        correct: "choice4"
+    },
+
+    {
+        question: "String values must be enclosed withing ______ when being assigned to variables.",
+        choice1: "commas",
+        choice2: "curly brackets",
+        choice3: "quotes",
+        choice4: "parenthesis",
+        correct: "choice3"
+    },
+
+    {
+        question: "A very useful tool used during development and debuggin for printing content to the debugger is:",
+        choice1: "JavaScript",
+        choice2: "terminal/bash",
+        choice3: "for loops",
+        choice4: "console.log",
+        correct: "choice4"
+    },
+
 ]
