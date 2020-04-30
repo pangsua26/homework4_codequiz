26ab84a14ce3ba66d4399271f256cfee5759b9aa
Pangsua Vang <pangsua26@yahoo.com>
2020-04-30 12:14:25 -0500

    added function to start quiz

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,16 @@
 // Select elements by ID
 
+var head = document.getElementById("start");
 var startQuiz = document.getElementById("startBtn")
 var quizEl = document.getElementById("quiz");
 var timerEl = document.getElementById("timer");
-var questionEl = document.getElementById("question");
+var questionEl = document.getElementById("questions");
 var choice1 = document.getElementById("choice1");
 var choice2 = document.getElementById("choice2");
 var choice3 = document.getElementById("choice3");
 var choice4 = document.getElementById("choice4");
 var response = document.getElementById("response");
+var index = 0
 
 
 
@@ -18,61 +20,79 @@ startQuiz.addEventListener("click", startBtn)
 // function to start the quiz
 function startBtn() {
     console.log("start")
-    startQuiz.classList.add("hide")
-    quizEl.classList.remove("hide")
+    head.style.display="none"
+    quizEl.style.display="block"
    
-// function to start timer counting down from 90 seconds
-    timerEl();
-    var countdown = setInterval(function() {
-        timer--;
-    })
+
 
 // Create questions and answers and correct answer
 var questions = [
     {
         question: "Commonly used data types DO NOT include:",
-        choice1: "strings",
-        choice2: "booleans",
-        choice3: "alerts",
-        choice4: "numbers",
-        correct: "choice4"
+        choice: [
+            "stings",
+            "booleans",
+            "alerts",
+            "numbers",
+        ],
+        correct:"alerts"
     },
 
     {
         question: "The condition in an if/else statement is enclosed within ______.",
-        choice1: "quotes",
-        choice2: "curly brackets",
-        choice3: "parenthesis",
-        choice4: "square brackets",
-        correct: "choice2"
+        choice: [
+            "quotes",
+            "curly brackets",
+            "parenthesis",
+            "square brackets",
+        ],
+        correct: "curly brackets"
     },
 
     {
         question: "Arrays in JavaScript can be used to store ______.",
-        choice1: "numbers and strings",
-        choice2: "other arrays",
-        choice3: "booleans",
-        choice4: "all the above",
-        correct: "choice4"
+        choice: [
+            "numbers and strings",
+            "other arrays",
+            "booleans",
+            "all the above",
+        ],
+            correct: "all the above"
     },
 
     {
         question: "String values must be enclosed withing ______ when being assigned to variables.",
-        choice1: "commas",
-        choice2: "curly brackets",
-        choice3: "quotes",
-        choice4: "parenthesis",
+        choice: [
+            "commas",
+            "curly brackets",
+            "quotes",
+            "parenthesis",
+        ],
         correct: "choice3"
     },
 
     {
         question: "A very useful tool used during development and debuggin for printing content to the debugger is:",
-        choice1: "JavaScript",
-        choice2: "terminal/bash",
-        choice3: "for loops",
-        choice4: "console.log",
-        correct: "choice4"
+        choice: [
+            "JavaScript",
+            "terminal/bash",
+            "for loops",
+            "console.log",
+        ],
+        correct: "console.log"
     },
+    
 ]
+console.log("index", questions[index].question);
+questionEl.textContent=questions[index].question;
 
+for (var i = 0; i < questions[index].choice.length; i++ ) {
+    console.log(questions[index].choice[i])
+}
+
+// function to start timer counting down from 90 seconds
+// timerEl();
+// var countdown = setInterval(function() {
+//     timer--;
+// })
 }
